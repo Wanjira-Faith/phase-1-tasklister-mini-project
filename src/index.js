be36f9b46e5c71aba2bc6fa042032b56ba7ea70f
+ <+>
2023-06-22 09:11:21 +0300

    Assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+const form=document.getElementById("create-task-form");
+form.addEventListener("submit",(e)=>{
+  e.preventDefault()
+  buildToDo(e.target.new-task-description.value)
+  form.reset()
+ })
+})
+
+function buildToDo(todo){
+  let li=document.createElement('li')
+  let btn=document.createElement('button')
+  btn.addEventListener("click", handleDelete)
+  btn.textContent=('x')
+  li.textContent=`${todo}`
+  li.appendChild(btn)
+
+  document.querySelector('#list').appendChild(li)
+  console.log(li)
+}
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
